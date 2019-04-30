dec5233cab98d8063e9d736dd97518984b2c3491
potashkeren <potashkeren@gmail.com>
2019-04-30 15:55:28 +0300

    collect tweets

---
+++ analyze_data.py
@@ -0,0 +1,30 @@
+import re
+from pymongo import MongoClient
+
+MONGOHOST = "mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=twitterdb"
+
+# override tweepy.StreamListener to add logic to on_status
+class DataExtraction():
+    def __init__(self,collection):
+        self.collection = collection
+
+    def get_tweets_by_social_network(self,social_network):
+        regex = re.compile(r"href=.*({}|{}).*>".format(social_network, social_network.capitalize()))
+        reg_entity = re.compile(r'{}|{}'.format(social_network, social_network.capitalize()))
+
+        res_source = list(self.collection.find({"source": regex}))
+        #res_entity =list(self.collection.find({"source":{"urls":{}} regex}))
+
+       # res_entity =
+        return reg_entity
+
+
+client = MongoClient(MONGOHOST)
+db = client.twitterdb
+collection = db.twitter_search
+
+d_extraction = DataExtraction(collection)
+pinterest_tweets = d_extraction.get_tweets_by_social_network("pinterest")
+
+print(len(pinterest_tweets))
+# print(pinterest_tweets)
--- listener.py
+++ listener.py
@@ -1,7 +1,10 @@
 import tweepy
 import json
 import re
-from requests import session
+from pymongo import MongoClient
+
+
+MONGOHOST = "mongodb://localhost:27017/?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&3t.uriVersion=3&3t.connection.name=twitterdb"
 
 
 # override tweepy.StreamListener to add logic to on_status
@@ -9,46 +12,46 @@ class MyStreamListener(tweepy.StreamListener):
     def on_status(self, status):
         print(status.text)
 
+    # Implements tweets filtering when data arrives
     def on_data(self, raw_data):
-        tweet_json = json.loads(raw_data)
-        r = re.compile(r"href=.*(instagram|pinterest|Instagram|Pinterest).*>")
-        test = r.search(tweet_json["source"])
-        if not test:
-            print(" Tweet was filtered....")
-            print(tweet_json["source"]+"\n")
-            return True
-        with open('fetched_tweets.txt','a') as tf:
-            print("Tweet Accepted !")
-            print(tweet_json["source"]+"\n")
-            tf.write(raw_data)
-        return True
+        try:
+            client = MongoClient(MONGOHOST)
+            db = client.twitterdb
+
+            tweet_json = json.loads(raw_data)
+            reg_source = re.compile(r"href=.*(instagram|pinterest|Instagram|Pinterest|facebook|Facebook|tumblr|Tumblr).*>")
+            reg_entity = re.compile(r'instagram|pinterest|Instagram|Pinterest|facebook|Facebook|tumblr|Tumblr')
+            result = None
+            for _json in tweet_json["entities"]["urls"]:
+                if result:
+                    break
+                result = reg_entity.search(str(_json))
+
+            if not result:
+                result = reg_source.search(tweet_json["source"])
+
+            if result:
+                print("Tweet Accepted {}".format(tweet_json["source"]))
+                db.social_tweets.insert(tweet_json)
+
+        except Exception as e:
+            print(e)
 
 
-
+# Credentials for twitter API
 CONSUMER_TOKEN ="GO3wNI7lcybeZucegHIuBu3BD"
 CONSUMER_SECRET ="tynoLttKAogFbjCLM6JGS32rQ6sYKPdisPtty4IAOXBZVpkn7H"
 ACCESS_TOKEN="1107293654291488768-gewQ3G53w0XXlJZRLRu2t0pAvH7EkU"
 ACCESS_TOKEN_SECRET="bhnrWLrivotga31WEphF3bPtLhqmVQIHP55snHSWxnWmX"
 
+# Twitter Authentication
 auth = tweepy.OAuthHandler(CONSUMER_TOKEN, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
-# search_results = api.search(q="instagram",lang="en")
+
+# Activate twitter streaming
 my_listener = MyStreamListener(api=api)
 stream = tweepy.Stream(auth=api.auth, listener=my_listener)
-stream.filter(track=["pinterest"],languages=["en"])
+stream.filter(track=["pinterest","facebook","instagram","tumblr"],languages=["en"])
 
-
-
-# for result in search_results:
-#     print(len(search_results))
-#     print("User:")
-#     print(result.user.name)
-#     print("Source:")
-#     print(result.source)
-#     print("Text:")
-#     print(result.text)
-#     print("Entities:")
-#     print(result.entities['urls'])
-#     print("****\n\n*****")
